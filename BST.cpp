091cdafcb2045bd19a7feab13ef5561c294ee59b
Tahrim19 <141277912+Tahrim19@users.noreply.github.com>
2023-10-15 01:01:55 +0500

    BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -7,76 +7,77 @@ struct BSTNode{
 };
 BSTNode* root = NULL;
 
-// node insertion in binary tree
-BSTNode* insertion(BSTNode* node , int value){
-	if(node == NULL){
-		BSTNode* temp = new BSTNode();
-		temp->data = value;
-		temp->left = NULL;
-		temp->right = NULL;
-		return temp;
-	}
-	if(value > node->data){
-		node->right = insertion(node->right , value);
-	}
-	else{
-		node->left = insertion(node->left , value);
-	}
-	return node;
+// Function to insert a node with the given 'value' into the binary search tree.
+BSTNode* insertion(BSTNode* node, int value) {
+    if (node == NULL) {  // If the current node is null, create a new node and return it.
+        BSTNode* temp = new BSTNode();
+        temp->data = value;
+        temp->left = NULL;
+        temp->right = NULL;
+        return temp;
+    }
+    if (value > node->data) {  // If 'value' is greater than the current node's data, insert it in the right subtree.
+        node->right = insertion(node->right, value);
+    } else {  // If 'value' is less than or equal to the current node's data, insert it in the left subtree.
+        node->left = insertion(node->left, value);
+    }
+    return node;  // Return the updated node with the inserted value.
 }
 
-void searching(BSTNode*node , int tosearch){
-	if(node == NULL){
-		cout<<"not found";
-	}
-	else if(node->data == tosearch){
-		cout<<"found";	
-	}
-	else if(tosearch > node->data && node->right != NULL){
-		searching(node->right , tosearch);
-	}
-	else if(tosearch < node->data && node->left != NULL){
-	searching(node->left , tosearch);
-	}
-	else{
-		cout<<"not found";
-	}
+
+// Function to search for a specific 'tosearch' value in the binary search tree.
+void searching(BSTNode* node, int tosearch) {
+    if (node == NULL) {
+        cout << "not found"; // If the current node is null, the value is not found in the tree.
+    } else if (node->data == tosearch) {
+        cout << "found"; // If the value matches the data in the current node, it's found.
+    } else if (tosearch > node->data && node->right != NULL) {
+        // If 'tosearch' is greater than the current node's data and the right subtree exists, search in the right subtree.
+        searching(node->right, tosearch);
+    } else if (tosearch < node->data && node->left != NULL) {
+        // If 'tosearch' is less than the current node's data and the left subtree exists, search in the left subtree.
+        searching(node->left, tosearch);
+    } else {
+        cout << "not found"; // If none of the above conditions match, the value is not found in the tree.
+    }
 }
 
-BSTNode* deletion(BSTNode* node , int todel){
-	if(node == NULL){
-		cout<<"empty";
-	}
-	if(todel < node->data){
-		node->left = deletion(node->left , todel);
-	}
-	else if(todel > node->data){
-		node->right = deletion(node->right , todel);
-	}
-	else{  // having one child
-		if(node->left == NULL){
-			BSTNode* temp = node->right;
-			delete(node);
-			return temp;
-		}
-		if(node->right == NULL){
-			BSTNode* temp = node->left;
-			delete(node);
-			return temp;
-		}
-		else{
-			// having two child , using smaller number approach.
-			BSTNode*temp = node->right;
-			// find smallest
-			while(temp->left != NULL){
-				temp = temp->left;
-			}
-			int small = temp->data;
-			node->data = small;
-			node->right = deletion(node->right , todel);
-		}
-	}
+
+// Function to delete a node with the specified 'todel' value from the binary search tree.
+BSTNode* deletion(BSTNode* node, int todel) {
+    if (node == NULL) {
+        cout << "empty"; // If the tree is empty, there's nothing to delete.
+    }
+    if (todel < node->data) {
+        node->left = deletion(node->left, todel);
+    } else if (todel > node->data) {
+        node->right = deletion(node->right, todel);
+    } else {  // Node to delete found
+        if (node->left == NULL) {
+            // If the node has no left child, replace it with its right child.
+            BSTNode* temp = node->right;
+            delete node; // Delete the current node.
+            return temp; // Return the right child to connect to the parent.
+        }
+        if (node->right == NULL) {
+            // If the node has no right child, replace it with its left child.
+            BSTNode* temp = node->left;
+            delete node; // Delete the current node.
+            return temp; // Return the left child to connect to the parent.
+        } else {
+            // If the node has two children, replace it with the smallest value from the right subtree.
+            BSTNode* temp = node->right;
+            while (temp->left != NULL) {
+                temp = temp->left;
+            }
+            int small = temp->data;
+            node->data = small; // Replace the current node's data with the smallest value.
+            node->right = deletion(node->right, todel); // Delete the node with the smallest value in the right subtree.
+        }
+    }
+    return node; // Return the updated node after deletion.
 }
+
 
 // In order traversal
 void InOrder(BSTNode* node){ // root node will be present in the middle
@@ -121,29 +122,6 @@ int Height(BSTNode* node){
 
 
 int main(){
-//	root = insertion(root,75);
-//	root = insertion(root ,85);
-//	root = insertion(root,25);
-//	root = insertion(root,5);
-//	root = insertion(root,95);
-//	PreOrder(root);
-//	cout<<endl;
-//	int search;
-//	cout<<"enter number to be searched.";
-//	cin>>search;
-//	searching(root,search);
-//	cout<<endl;
-//	int todel;
-//	cout<<"enter number to be delete.";
-//	cin>>todel;
-//	root = deletion(root,todel);
-//	cout<<"the value "<<todel<< "is deleted." << endl;
-//	cout<<endl;
-//	PreOrder(root);
-//	cout<<endl;
-//	Height(root);
-//	return 0;
-
 	int op;
 	char choice = 'y';
 	while (choice == 'y'){
@@ -195,7 +173,4 @@ int main(){
 	cout<<"do you want to continue? y/n";
 	cin>>choice;
 }
-
-
 }
-
